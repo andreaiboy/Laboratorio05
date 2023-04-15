439e79359f7f3964a3a43fc14dd776f7a096cc76
Andrea <andreagiboy@gmail.com>
2023-04-14 21:27:53 -0600

    realizar program

--- Laboratorio05/Equipo.cs
+++ Laboratorio05/Equipo.cs
@@ -1,62 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Laboratorio05
+﻿namespace Laboratorio05
 {
     public class Equipo
     {
         private string Nombre;
-        private int PartidoGanados;
-        private int PartidoEmpatados;
-        private int PartidoPerdidos;
+        private int PartidosGanados;
+        private int PartidosEmpatados;
+        private int PartidosPerdidos;
         private int GolesFavor;
         private int GolesContra;
+
         public Equipo(string nombre, int partidosGanados, int partidosEmpatados, int partidosPerdidos, int golesFavor, int golesContra)
         {
-            if (partidosEmpatados + partidosGanados + partidosPerdidos != 3 ||
-                golesFavor <0 || golesContra < 0)
-                    {
+            if (partidosEmpatados + partidosGanados + partidosPerdidos != 3 || golesContra < 0 || golesFavor < 0)
                 throw new Exception("Revise el equipo. La sumatoria de partidos debe ser igual a 3 y los goles anotados y a favor deben ser mayores o iguales a cero.");
-            }
+
             this.Nombre = nombre;
-            this.PartidoGanados= partidosGanados;
-            this.PartidoEmpatados= partidosEmpatados;
-            this.PartidoPerdidos= partidosPerdidos;
-            this.GolesFavor= golesFavor;
+            this.PartidosGanados = partidosGanados;
+            this.PartidosEmpatados = partidosEmpatados;
+            this.PartidosPerdidos = partidosPerdidos;
+            this.GolesFavor = golesFavor;
             this.GolesContra = golesContra;
         }
 
         public string GetNombre()
         {
-            return this.Nombre;
+            return Nombre;
         }
 
         public int GetPartidosGanados()
         {
-            return this.PartidoGanados;
+            return PartidosGanados;
         }
 
         public int GetPartidosEmpatados()
         {
-            return this.PartidoEmpatados;
+            return PartidosEmpatados;
         }
 
         public int GetPartidosPerdidos()
         {
-            return this.PartidoPerdidos;
+            return PartidosPerdidos;
         }
 
         public int GetGolesFavor()
         {
-            return this.GolesFavor;
+            return GolesFavor;
         }
 
         public int GetGolesContra()
         {
-            return this.GolesContra;
+            return GolesContra;
         }
-    
+    }
 }
--- Laboratorio05/Program.cs
+++ Laboratorio05/Program.cs
@@ -4,8 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Equipo equipo1 = new Equipo("PSG", 3, 0, 1, 2, 0);
-            Equipo equipo2 = new Equipo("Barcelona", 0, 3, 2, 1, 3);
+            Equipo equipo1 = new Equipo("PSG", 3, 0, 0, 1, 0);
+            Equipo equipo2 = new Equipo("Barcelona", 3, 0, 0, 1, 0);
+
+            Partido partido = new Partido(equipo1, equipo2);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(partido.SeleccionarEquipoGanador().GetNombre());
+            }
         }
     }
 }
+       
