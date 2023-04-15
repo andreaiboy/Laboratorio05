fcccfaf373ea930991c80cb842520ab7ae14e0df
Andrea <andreagiboy@gmail.com>
2023-04-14 20:25:28 -0600

    realizar random generator

--- Laboratorio05/Information.cs
+++ Laboratorio05/Information.cs
@@ -18,9 +18,7 @@ namespace Laboratorio05
         public static Information GetInformation()
         {
             if (Instance == null)
-                Instance = new Information("Andrea Gabriela Iboy Casados", "1076623", "04"
-                                           "Ana Lucía Chávez Penagaos", "1067423", "04" 
-                                           "Eduardo David Hernández Girón", "1156823", "04");
+                Instance = new Information("Grupo 02", "1076623 1067423 1156823", "04");
 
             return Instance;
         }
